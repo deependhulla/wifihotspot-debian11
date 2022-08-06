b88ed6045672029e2b3b1b06e7b42153bb57a7f8
Deepen Dhulla <deepen@deependhulla.com>
2022-08-06 14:49:36 +0530

    update on Sat Aug  6 14:49:36 IST 2022

--- 03-admin-packages.sh
+++ 03-admin-packages.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 ## for Development of checking Database in details.
-
 wget -c "https://files.phpmyadmin.net/phpMyAdmin/5.1.3/phpMyAdmin-5.1.3-english.tar.gz" -O /tmp/phpMyAdmin-5.1.3-english.tar.gz
 cd /tmp
 tar -xzf phpMyAdmin-5.1.3-english.tar.gz 
@@ -11,11 +10,11 @@ cp /var/www/html/dbadminonweb/config.sample.inc.php /var/www/html/dbadminonweb/c
 chown -R www-data:www-data /var/www/html/dbadminonweb
 sed -i "s/\$cfg\['blowfish_secret'\] = '';/\$cfg\['blowfish_secret'\] = '`pwgen -c 32 1`';/" /var/www/html/dbadminonweb/config.inc.php
 
-
+## for vpn/sd-wan setup
 curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
 if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
 
-
+## key package for iptables management
 echo "deb https://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list 
 wget -c https://download.webmin.com/jcameron-key.asc -O /etc/apt/trusted.gpg.d/webmin-jcameron-key.asc
 apt-get update
@@ -23,4 +22,4 @@ apt-get -y install webmin
 ## change port from 10000 to 8383
 sed -i "s/10000/8383/g" /etc/webmin/miniserv.conf
 /etc/init.d/webmin restart 2>/dev/null
-echo "Webmin run https on port 8383 use Firefox Browser to Access not Google Chrome as SSL Certifcate is not applied yet"; 
+#echo "Webmin run https on port 8383 use Firefox Browser to Access not Google Chrome as SSL Certifcate is not applied yet"; 
