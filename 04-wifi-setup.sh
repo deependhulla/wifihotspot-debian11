38e21f3652e39b4a2bd37d94b29359fa4fb38aca
Deepen Dhulla <deepen@deependhulla.com>
2023-07-11 17:04:58 +0530

    update on Tue Jul 11 05:04:58 PM IST 2023

--- 04-wifi-setup.sh
+++ 04-wifi-setup.sh
@@ -23,6 +23,20 @@ systemctl start named
 systemctl enable named
 
 
+
+### changing timezone to Asia Kolkata
+sed -i "s/;date.timezone =/date\.timezone \= \'Asia\/Kolkata\'/" /etc/php/7.4/apache2/php.ini
+sed -i "s/;date.timezone =/date\.timezone \= \'Asia\/Kolkata\'/" /etc/php/7.4/cli/php.ini
+sed -i "s/;date.timezone =/date\.timezone \= \'Asia\/Kolkata\'/" /etc/php/7.4/fpm/php.ini
+##disable error
+sed -i "s/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ERROR/" /etc/php/7.4/cli/php.ini
+sed -i "s/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ERROR/" /etc/php/7.4/fpm/php.ini
+sed -i "s/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ERROR/" /etc/php/7.4/apache2/php.ini
+
+sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.4/apache2/php.ini
+sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.4/cli/php.ini
+sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.4/fpm/php.ini
+
 sed -i "s/Geng0yoo/`cat /usr/local/src/mariadb-mydbadmin-pass`/" /var/www/html/wifiadmin/dbinfo.php
 sed -i "s/Geng0yoo/`cat /usr/local/src/mariadb-mydbadmin-pass`/" /var/www/html/wifilogin/dbconfig.php
 
@@ -30,6 +44,7 @@ sed -i "s/Geng0yoo/`cat /usr/local/src/mariadb-mydbadmin-pass`/" /usr/share/webm
 sed -i "s/Geng0yoo/`cat /usr/local/src/mariadb-mydbadmin-pass`/" /usr/share/webmin/wifidirect/get-data-usage-per-mac.pl
 sed -i "s/Geng0yoo/`cat /usr/local/src/mariadb-mydbadmin-pass`/" /usr/share/webmin/wifidirect/update-daily-summmary-for-bytes.pl 
 
+ln -vs /usr/share/webmin /usr/local/webadmin
 
 
 systemctl enable isc-dhcp-server.service
